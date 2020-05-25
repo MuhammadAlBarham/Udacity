1af29893fc06fa41f75e35063e9a1dda1765593a
Muhammad Al-Barham <mohammedbrham98@gmail.com>
2020-05-25 22:53:49 +0300

    Add Join_Questoin

---
+++ Data Analysis Track/Basic SQL/JOIN_Questions.sql
@@ -0,0 +1,29 @@
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+WHERE a.name = 'Walmart';
+
+
+/************************************************/
+
+SELECT r.name region, s.name rep, a.name account
+FROM sales_reps s
+JOIN region r
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+ORDER BY a.name;
+
+
+/*****************************************************/
+
+SELECT r.name region, a.name account, 
+       o.total_amt_usd/(o.total + 0.01) unit_price
+FROM region r
+JOIN sales_reps s
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id;
